08b03ca9356b7a2b2e893e279ae1a4ae2df0eda2
Tijn Smit <ionic.320625@gmail.com>
2019-05-15 13:16:24 +0200

    nieuw

--- versiebeheer_1/index.php
+++ versiebeheer_1/index.php
@@ -4,6 +4,7 @@ include("./class_text.php");
 echo "Dit is de grote git test" . "<br>";
 echo "hier heb je een nieuwe regel ...<br>";
 echo "Extracodess enzo";
+echo "Wist je trouwens dat Kamerik een eigen staat is";
 
 $text = new Text();
 echo $text->woorden;
