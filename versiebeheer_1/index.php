05053995daebcb6a4eb7a4c615ab0ba20d4f8883
Tijn Smit <ionic.320625@gmail.com>
2019-05-22 13:12:44 +0200

    freesyle

--- versiebeheer_1/index.php
+++ versiebeheer_1/index.php
@@ -5,6 +5,7 @@ echo "Dit is de grote git test" . "<br>";
 echo "hier heb je een nieuwe regel ...<br>";
 echo "Extracodess enzo";
 echo "Wist je trouwens dat Kamerik een eigen staat is";
+echo "frestyle comit";
 
 $text = new Text();
 echo $text->woorden;
